ea65b7fc7938d99aa34f7e57827d198dd22ff5c4
Nasif Ahmed <nasif.ahmed@live.com>
2023-09-19 19:34:59 +0600

    module 2 assignment

--- module2/task1.php
+++ module2/task1.php
@@ -1,7 +1,8 @@
 <?php
+declare(strict_types=1);
 // for loop
 // ============================================
-function printEvenNumbers($start,$end,$step){
+function printEvenNumbers(int $start, int $end, int $step):void{
     for($i = $start+1; $i <= $end; $i+=$step){
         if($i % 2 == 0){
             echo "{$i} ";
@@ -12,7 +13,7 @@ printEvenNumbers(1,20,2);
 
 // while loop
 // ============================================
-// function printEvenNumbers($start,$end,$step){
+// function printEvenNumbers(int $start, int $end, int $step):void{
 //     $start++;
 //     while($start <= $end){
 //         if($start % 2 == 0){
@@ -23,10 +24,9 @@ printEvenNumbers(1,20,2);
 // }
 // printEvenNumbers(1,20,2);
 
-
 // do while loop
 // ============================================
-// function printEvenNumbers($start,$end,$step){
+// function printEvenNumbers(int $start, int $end, int $step):void{
 //     $start++;
 //     do{
 //         if($start % 2 == 0){
